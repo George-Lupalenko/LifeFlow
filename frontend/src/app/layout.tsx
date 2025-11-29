bb679aeb4744a6c713ac1ee8c8322c6b0342f909
Kasper1no <slobodian666@gmail.com>
2025-11-29 22:33:57 +0200

    Resolve merge conflict with remote

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
+import { SettingsProvider } from "@/context/settings/SettingsContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
