6f9278132eb54893131ad7cf94bfd096a5fe0aab
Danylo Senchyshyn <danilsencisin@gmail.com>
2025-11-29 14:57:04 +0100

    added bank module

---
+++ bank-statement-analytics/src/main/java/com/bankstatementanalytics/BankStatementAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.bankstatementanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStatementAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankStatementAnalyticsApplication.class, args);
+    }
+
+}
---
+++ bank-statement-analytics/src/test/java/com/bankstatementanalytics/BankStatementAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankstatementanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankStatementAnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
