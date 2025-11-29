7dbd315395885e526c68bd6f75d6a603a5028618
Kasper1no <slobodian666@gmail.com>
2025-11-29 20:28:50 +0200

    initial frontend setup

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header/Header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "LifeFlow",
+  description: "AI-assistent for your life",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-auto overflow-x-hidden flex flex-col min-h-screen`}
+      >
+        <Header />
+        <div className="flex-1 flex flex-col overflow-auto">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
